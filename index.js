5d262a484d01c97dad92c83ef3355d7e5966da58
cal1forn1aSoul <jonathan.a.atia@gmail.com>
2020-11-05 23:09:34 +0200

    added async/await

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+#! /usr/bin/env node
 const { exec } = require('child_process');
 const { writeFile } = require('fs');
 
-const index =`<!DOCTYPE html>
+let fileName = process.argv.slice(2)[0];
+// let title = fileName.slice(0,1).toUpperCase() + fileName.slice(1);
+
+const index = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <script src="index.js" defer></script>
-  <title>Document</title>
+  <title>${fileName}</title>
 </head>
 <body>
     
 </body>
 </html>`;
 
-exec('touch index.js style.css');
-writeFile('./index.html', index, err => err ? console.log(err) : null);
+async function generateProject() {
+  await exec(`mkdir ${fileName}`);
+  await exec(`touch ${fileName}/index.js ${fileName}/style.css`);
+  await writeFile(`./${fileName}/index.html`, index, err => (err ? console.log(err) : null));
+}
+
+generateProject();
